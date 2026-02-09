4d28500a0598cc7a77669a5d18d2d934503532d3
Desm00nt <deniska.kartuzov@gmail.com>
2026-02-09 16:26:23 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', function() {
+    console.log('Clicker Game running on port ' + PORT);
+});
